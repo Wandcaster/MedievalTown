a30b6bbb66065e5947b72c217f1930ed2534d6f5
Degern1024 <102228685+Degern1024@users.noreply.github.com>
2022-05-10 16:55:59 +0200

    Debugowanie generowania lochu
    Naprawiony bug z NullReferenceException, wielokrotnym boss room'em

--- Assets/My assets/Radek/DungeonManager.cs
+++ Assets/My assets/Radek/DungeonManager.cs
@@ -22,7 +22,7 @@ public class DungeonManager : MonoBehaviour
     //Delete it when you serialize it's contents in GenerateDungeon.cs
     private void Awake()
     {
-        roomsWithoutDescendingArea = 5;
+        roomsWithoutDescendingArea = 3;
         chanceToSpawnDescendingArea = 5;
     }
 
@@ -61,131 +61,144 @@ public class DungeonManager : MonoBehaviour
         
     }
 
-    private void clearChild(GameObject transition)
+    private void ClearChild(GameObject transition)
     {
         foreach (Transform child in transition.transform)
         {
             Destroy(child.gameObject);
+            
         }
+        transition.transform.DetachChildren();//ta linijka nie ma sensu, ale aktualizuje referencje do gameObject'ow
+                                              //przez co skrypt sie nie wywala jak natrafi na generowanie pokoju 
+                                              //krotszego pokoju od poprzedniego
     }
     
 
-    private void GenerateDung2(GameObject transition, int remainingLength, bool hasBoosRoom, GameObject room, bool clearParent)
+    private void GenerateDung2(GameObject transition, int remainingLength, bool hasBossRoom, GameObject room, bool clearParent)
     {
-        Vector3 transitionPos = transition.transform.position;
-        if(clearParent)clearChild(transition);
-        int roomsToGenerateWithoutDescendingArea;
-        if (remainingLength > roomsWithoutDescendingArea) roomsToGenerateWithoutDescendingArea = roomsWithoutDescendingArea;
-        else roomsToGenerateWithoutDescendingArea = remainingLength;
-        Debug.Log(remainingLength);
         
-        int i;
+            Vector3 transitionPos = transition.transform.position;
+            //if (clearParent) clearChild(transition);
+            int roomsToGenerateWithoutDescendingArea;
+            if (remainingLength > roomsWithoutDescendingArea) roomsToGenerateWithoutDescendingArea = roomsWithoutDescendingArea;
+            else roomsToGenerateWithoutDescendingArea = remainingLength;
+            //Debug.Log(remainingLength);
 
-        for (i=0; i < roomsToGenerateWithoutDescendingArea; i++)
-        {
-            transitionPos = AppendRoom(
-            room.transform.Find("Room" + Random.Range(0, GetRooms())).gameObject,
-            transition, i, transitionPos);
+            int i=0;
 
-        }
-        remainingLength -= roomsToGenerateWithoutDescendingArea;
-        System.Random rand = new System.Random();
 
-        do
-        {
-
-            float randomFloat = (float)rand.NextDouble();//[0;1]
-            float chances = (float) 1 / chanceToSpawnDescendingArea;
-            remainingLength--;
-
-            //if true, then create descendingArea; can't do when remaining length is 1;
-            if (remainingLength > 1)
+            for (i = 0; i < roomsToGenerateWithoutDescendingArea; i++)
             {
-                //Debug.Log(randomFloat.ToString() + "<" + chances.ToString());
-                if (randomFloat < chances)
-                {
-                    //descendArea
-                    transitionPos = AppendRoom(
-                  room.transform.parent.Find("DescendingAreas").Find("Room" + Random.Range(0, GetFinalRooms())).gameObject,
-                  transition, i, transitionPos);
-                    //odpal rekurencyjnie 2x
-                    GameObject tmp = transition.transform.Find("GeneratedRoom" + i).gameObject;
-
-                    int length1;
-                    int length2;
-                    if (remainingLength == 2) { length1 = 1;length1 = 1; }
-                    else
-                    {
-                        length1 = UnityEngine.Random.Range(1, remainingLength-1);
-                        length2 = remainingLength - length1;
-                        Debug.Log(tmp.name);
-
-                        GenerateDung2(tmp.transform.Find("Transition2").gameObject, length1, length1 >= length2, room, false);
-                        GenerateDung2(tmp.transform.Find("Transition3").gameObject, length2, length1 < length2, room, false);
-
-
-
-                        //Debug.Log("l1=" + length1.ToString() + ";l2=" + length2.ToString());
-                    }
-
-                    break;
-                }
-                else
-                {
-                    //Room
-                    transitionPos = AppendRoom(
-                room.transform.Find("Room" + Random.Range(0, GetRooms())).gameObject,
-                transition, i, transitionPos);
-                }
-
-            }
-            else if (remainingLength == 1)
-            {
-                //Room
                 transitionPos = AppendRoom(
                 room.transform.Find("Room" + Random.Range(0, GetRooms())).gameObject,
                 transition, i, transitionPos);
+
             }
-            else //remainingLength==0
+            remainingLength -= roomsToGenerateWithoutDescendingArea;
+
+        System.Random rand = new System.Random();
+        int chanceToSpawnDescendingAreaButLocal = chanceToSpawnDescendingArea;
+        do
+        {
+            try
             {
-                if (hasBoosRoom)
+                float randomFloat = (float)rand.NextDouble();//[0;1]
+                float chances = (float)1 / chanceToSpawnDescendingAreaButLocal;
+                remainingLength--;
+
+                //if true, then create descendingArea; can't do when remaining length is 1;
+                if (remainingLength > 1)
                 {
-                    //restRoom and bossRoom
+                    //Debug.Log(randomFloat.ToString() + "<" + chances.ToString());
+                    if (randomFloat < chances)
+                    {
+                        //descendArea
+                        transitionPos = AppendRoom(
+                      room.transform.parent.Find("DescendingAreas").Find("Room" + Random.Range(0, GetFinalRooms())).gameObject,
+                      transition, i, transitionPos);
+                        //odpal rekurencyjnie 2x
+                        GameObject tmp = transition.transform.Find("GeneratedRoom" + i).gameObject;
+
+                        int length1;
+                        int length2;
+                        if (remainingLength == 2) { length1 = 1; length2 = 1; }
+                        else
+                        {
+                            length1 = UnityEngine.Random.Range(1, remainingLength - 1);
+                            length2 = remainingLength - length1;
+                            //Debug.Log(tmp.name);
+                        }
+                        //Debug.Log(tmp.name+";"+(length1 >= length2).ToString()+";"+(!(length1 >= length2)).ToString());
+                        GenerateDung2(tmp.transform.Find("Transition2").gameObject, length1, hasBossRoom && length1 >= length2, room, false);
+                        GenerateDung2(tmp.transform.Find("Transition3").gameObject, length2, hasBossRoom && !(length1 >= length2), room, false);
+                        remainingLength = 0;
+                        break;
+                        //Debug.Log("dzialam pomimo break'a");
+                    }
+                    else
+                    {
+                        //Room
+                        transitionPos = AppendRoom(
+                    room.transform.Find("Room" + Random.Range(0, GetRooms())).gameObject,
+                    transition, i, transitionPos);
+                    }
+
+                }
+                else if (remainingLength == 1)
+                {
+                    //Room
                     transitionPos = AppendRoom(
-                           room.transform.parent.Find("RestRooms").Find("Room" + Random.Range(0, GetRestRooms())).gameObject,
-                           transition, i++, transitionPos);
+                    room.transform.Find("Room" + Random.Range(0, GetRooms())).gameObject,
+                    transition, i, transitionPos);
+                    //Debug.Log("To ja sie odpalam wiele razy");
+                }
+                else //remainingLength==0
+                {
+                    if (hasBossRoom)
+                    {
+                        //restRoom and bossRoom
+                        transitionPos = AppendRoom(
+                               room.transform.parent.Find("RestRooms").Find("Room" + Random.Range(0, GetRestRooms())).gameObject,
+                               transition, i++, transitionPos);
 
 
-                    transitionPos = AppendRoom(
-                   room.transform.parent.Find("FinalRooms").Find("Room" + Random.Range(0, GetFinalRooms())).gameObject,
-                   transition, i, transitionPos);
+                        transitionPos = AppendRoom(
+                       room.transform.parent.Find("FinalRooms").Find("Room" + Random.Range(0, GetFinalRooms())).gameObject,
+                       transition, i, transitionPos);
+                    }
+                    else
+                    {
+                        //deadEndRoom
+                        transitionPos = AppendRoom(
+                       room.transform.parent.Find("DeadEndRooms").Find("Room" + Random.Range(0, GetDeadEndRooms())).gameObject,
+                       transition, remainingLength, transitionPos);
+                    }
+                    break;
                 }
-                else
-                {
-                    //deadEndRoom
-                    transitionPos = AppendRoom(
-                   room.transform.parent.Find("DeadEndRooms").Find("Room" + Random.Range(0, GetDeadEndRooms())).gameObject,
-                   transition, remainingLength, transitionPos);
-                }
-                break;
+
+
+
+
+
+
+                chanceToSpawnDescendingAreaButLocal--;
+                i++;
             }
+            catch (System.Exception e)
+            {
+                Debug.Log(e);
+                //Debug.Log("Bug2");
+            }
+        } while (remainingLength != 0) ;
 
-
-
-
-
-
-            chanceToSpawnDescendingArea--;
-            i++;
-        } while (remainingLength != 0);
     }
 
 
 
     private void GenerateDung(GameObject transition, DungeonGenerateChances chances)
     {
-        int remainingLength = GenerateLength(chances);
-        GenerateDung2(transition, remainingLength, true, gameObject.transform.Find("Presets").Find("Rooms").gameObject, true);
+        ClearChild(transition);
+        GenerateDung2(transition, GenerateLength(chances), true, gameObject.transform.Find("Presets").Find("Rooms").gameObject, true);
     }
 
 
