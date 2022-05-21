5bf3d9447c921f82a634b60040fdc072bc8d27fa
Degern1024 <102228685+Degern1024@users.noreply.github.com>
2022-05-21 17:51:03 +0200

    Modyfikacja GenerateDungeon
    Dungeon jest tworzony z uwzględnieniem braku miejsca na nowe pokoje

--- Assets/My assets/Radek/CheckArea.cs
+++ Assets/My assets/Radek/CheckArea.cs
@@ -8,13 +8,12 @@ public class CheckArea : MonoBehaviour
     [Tooltip("Pó³ rozmiaru w ka¿dym keirunku")]
     [SerializeField] Vector3 halfExtents;
 
-    //zwraca true, gdy obiekt z czymœ koliduje
+    //zwraca false, gdy obiekt z czymœ koliduje
     public bool SafeToPlace()
     {
-        Debug.Log(gameObject.transform.position + "|" + middle + "|" + halfExtents);
+        //Debug.Log(gameObject.transform.position + "|" + middle + "|" + halfExtents);
 
-
-        return !Physics.CheckBox(middle, halfExtents);
+        return !Physics.CheckBox(gameObject.transform.position+middle, halfExtents-new Vector3(0.01f, 0.001f, 0.01f));
     }
 
 }
--- Assets/My assets/Radek/DungeonManager.cs
+++ Assets/My assets/Radek/DungeonManager.cs
@@ -91,17 +91,22 @@ public class DungeonManager : MonoBehaviour
         //for (int j = 0; j < availableRooms.Count; j++) Debug.Log(availableRooms[j].name);
 
         //Debug.Break();
+        List<GameObject> availableRooms;
+
 
         //generowanie pokoi bez przy³¹czania/generowania pokoju prowadz¹cego w dó³
             for (i = 0; i < roomsToGenerateWithoutDescendingArea; i++)
             {
-            //availableRooms = GetAvailableRooms(room, transition);
-            transitionPos = AppendRoom(
-                GetAvailableRooms(room, transition),
-                transition, i, transitionPos);
-
+                //availableRooms = GetAvailableRooms(room, transition);
+                availableRooms = GetAvailableRooms(room, transition, transitionPos);
+                if (availableRooms.Count != 0) transitionPos = AppendRoom(availableRooms, transition, i, transitionPos);
+                else
+                {
+                //przerywa wykonywanie
+                    return;
+                }
             }
-            remainingLength -= roomsToGenerateWithoutDescendingArea;
+        remainingLength -= roomsToGenerateWithoutDescendingArea;
 
         //szansa na wygenerowanie pokoju prowadz¹cego w dó³, wzór: 1/n; n jest zmniejszane z ka¿d¹ iteracj¹
         System.Random rand = new System.Random();
@@ -124,7 +129,7 @@ public class DungeonManager : MonoBehaviour
                         //GetAvailableRooms(room.transform.parent.Find(roomNames.GetDescendingAreas()), transition);
                         //descendArea
                         transitionPos = AppendRoom(
-                             GetAvailableRooms(room.transform.parent.Find(roomNames.GetDescendingAreas()).gameObject, transition),
+                             GetAvailableRooms(room.transform.parent.Find(roomNames.GetDescendingAreas()).gameObject, transition, transitionPos),
                             transition, i, transitionPos);
 
                         GameObject tmp = transition.transform.Find(roomNames.GetGeneratedRoom() + i).gameObject;
@@ -163,7 +168,7 @@ public class DungeonManager : MonoBehaviour
                     {
                         //Room
                         transitionPos = AppendRoom(
-                    GetAvailableRooms(room, transition),
+                    GetAvailableRooms(room, transition, transitionPos),
                     transition, i, transitionPos);
                     }
 
@@ -173,7 +178,7 @@ public class DungeonManager : MonoBehaviour
                 {
                     //Room
                     transitionPos = AppendRoom(
-                    GetAvailableRooms(room, transition),
+                    GetAvailableRooms(room, transition, transitionPos),
                     transition, i, transitionPos);
                 }
                 //gdy pozosta³a d³ugoœæ lochu wynosi 0 to nastêpny pokój jest albo rest room z boss room'em, albo pokój bez wyjœcia
@@ -184,19 +189,19 @@ public class DungeonManager : MonoBehaviour
                         //restRoom and bossRoom
                         //GetAvailableRooms(room.transform.parent.Find(roomNames.GetRestRooms()).gameObject)
                         transitionPos = AppendRoom(
-                               GetAvailableRooms(room.transform.parent.Find(roomNames.GetRestRooms()).gameObject, transition),
+                               GetAvailableRooms(room.transform.parent.Find(roomNames.GetRestRooms()).gameObject, transition, transitionPos),
                                transition, i++, transitionPos);
 
 
                         transitionPos = AppendRoom(
-                       GetAvailableRooms(room.transform.parent.Find(roomNames.GetFinalRooms()).gameObject, transition),
+                       GetAvailableRooms(room.transform.parent.Find(roomNames.GetFinalRooms()).gameObject, transition, transitionPos),
                        transition, i, transitionPos);
                     }
                     else
                     {
                         //deadEndRoom
                         transitionPos = AppendRoom(
-                       GetAvailableRooms(room.transform.parent.Find(roomNames.GetDeadEndRooms()).gameObject, transition),
+                       GetAvailableRooms(room.transform.parent.Find(roomNames.GetDeadEndRooms()).gameObject, transition, transitionPos),
                        transition, remainingLength, transitionPos);
                     }
                     break;
@@ -227,22 +232,22 @@ public class DungeonManager : MonoBehaviour
         GenerateDungeon(transition, GenerateLength(chances), true, gameObject.transform.Find("Presets").Find("Rooms").gameObject, true);
     }
 
-    private List<GameObject> GetAvailableRooms(GameObject parentFolder, GameObject transitionPos)
+    private List<GameObject> GetAvailableRooms(GameObject parentFolder, GameObject parent, Vector3 transitionPos)
     {
         List<GameObject> output = new List<GameObject>();
         //Debug.Log("childCount " + parentFolder.transform.childCount);
         for(int i=0; i < parentFolder.transform.childCount; i++)
         {
-            if (CheckRoom(parentFolder.transform.GetChild(i).gameObject,transitionPos.transform.position, transitionPos))
+            if (CheckRoom(parentFolder.transform.GetChild(i).gameObject, transitionPos, parent))
             {
                 output.Add(parentFolder.transform.GetChild(i).gameObject);
             }
             //output.Add(parentFolder.transform.GetChild(i).gameObject);
         }
-        if(output.Count==0)
-        {
-            Debug.Log("Count0");
-        }
+        //if(output.Count==0)
+        //{
+        //    Debug.Log("Count0");
+        //}
         return output;
     }
 
@@ -254,7 +259,7 @@ public class DungeonManager : MonoBehaviour
     private Vector3 AppendRoom(List<GameObject> availableRooms, GameObject parent, int generatedRoomID, Vector3 transitionPos)
     {
         //GameObject room = gameObject.transform.Find("Presets").Find("Rooms").Find("Room" + roomID).gameObject;
-        if (availableRooms.Count == 0) return transitionPos;
+        if (availableRooms.Count == 0) { return transitionPos; }
 
 
         GameObject obj = Instantiate
@@ -288,7 +293,6 @@ public class DungeonManager : MonoBehaviour
     //sprawdza czy pokój koliduje z innym
     private bool CheckRoom(GameObject room, Vector3 transitionPos, GameObject parent)
     {
-        //tworze pusty GameObject, aby z niego wziac pozniej poprawne rotation
         GameObject obj = Instantiate
             (room, parent.transform);
         obj.SetActive(true);
@@ -306,17 +310,19 @@ public class DungeonManager : MonoBehaviour
         bool output = true;
         for (int i = 0; i < checkArea.transform.childCount; i++)
         {
-            
-
-
-            if (!checkArea.transform.GetChild(i).GetComponent<CheckArea>().SafeToPlace()) output = false;
+            //Debug.Log(checkArea.transform.GetChild(i).GetComponent<CheckArea>().SafeToPlace());
+            if (checkArea.transform.GetChild(i).GetComponent<CheckArea>().SafeToPlace() == false)
+            { 
+                output = false;
+                break;
+            }
         }
 
 
 
 
 
-        //Destroy(obj);
+        Destroy(obj);
         return output;
     }
    
